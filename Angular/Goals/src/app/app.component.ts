5eb58515725893da422a7862b221ab09b98f615c
lilian waweru <lilowesh.lw@gmail.com>
2019-03-21 17:56:12 +0300

    my goals

---
+++ Angular/Goals/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {Goal} from './goal'
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+ 
+  // title = 'Watch Finding Nemo ';
+}
