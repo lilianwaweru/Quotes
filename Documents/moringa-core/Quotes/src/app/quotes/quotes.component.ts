6653617c5698921f21b155c81f14cf8824dc0a97
lilian waweru <lilowesh.lw@gmail.com>
2019-03-22 17:51:05 +0300

    added pipes

--- Documents/moringa-core/Quotes/src/app/quotes/quotes.component.ts
+++ Documents/moringa-core/Quotes/src/app/quotes/quotes.component.ts
@@ -7,9 +7,9 @@ import {Quotes} from '../quotes'
 })
 export class QuotesComponent implements OnInit {
   quotes = [
-    new Quotes(1, 'here i am', 'Author:njuguna', 'Publisher:her'),
-    new Quotes(2, 'where are you', 'Author:muthoni', 'Publisher:his'),
-    new Quotes(3, 'yes am here', 'Author:waweru', 'Publisher:his'),
+    new Quotes(1, 'here i am', 'Author:njuguna', 'Publisher:her', new Date(2019,3,22)),
+    new Quotes(2, 'where are you', 'Author:muthoni', 'Publisher:his', new Date(2019,3,22)),
+    new Quotes(3, 'yes am here', 'Author:waweru', 'Publisher:his', new Date(2019,3,22)),
   ]
 
   quoteComplete(isComplete,index){
